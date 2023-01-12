d5663758bcf86516b0e79736c1de3d41fc9e55ce
Heshan Padmasiri <hpheshan@gmail.com>
2023-01-12 17:03:37 +0530

    Fix formatting

--- build.rs
+++ build.rs
@@ -1,10 +1,18 @@
-use std::{process::Command, path::PathBuf};
+use std::{path::PathBuf, process::Command};
 
 fn main() {
-    Command::new("rm").args(["-rf", "./content"]).status().unwrap();
+    Command::new("rm")
+        .args(["-rf", "./content"])
+        .status()
+        .unwrap();
     Command::new("mkdir").args(["content"]).status().unwrap();
-    Command::new("npm").args(["run", "build"]).current_dir(
-        PathBuf::from("./pea-client")
-    ).status().unwrap();
-    Command::new("cp").args(["-r", "./pea-client/build/", "./content"]).status().unwrap();
+    Command::new("npm")
+        .args(["run", "build"])
+        .current_dir(PathBuf::from("./pea-client"))
+        .status()
+        .unwrap();
+    Command::new("cp")
+        .args(["-r", "./pea-client/build/", "./content"])
+        .status()
+        .unwrap();
 }
