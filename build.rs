9f40ace023f12dfd466cc009f4aca29cbe1e19ee
Heshan Padmasiri <hpheshan@gmail.com>
2023-01-13 18:00:08 +0530

    Add ability for server to figure out ip address

--- build.rs
+++ build.rs
@@ -4,6 +4,8 @@ fn main() {
     let client_content_dir = "client-content";
     let content_dir = "content";
 
+    let local_ip = get_local_ip_address();
+
     clean_and_create_dir(client_content_dir);
     clean_and_create_dir(content_dir);
     create_and_copy_static_page("pea-client", client_content_dir)
@@ -27,4 +29,24 @@ fn create_and_copy_static_page(src: &str, dest: &str) {
         .args(["-r", &format!("./{}/build/", src), &format!("./{}", dest)])
         .status()
         .unwrap();
+}
+
+// copy of same function in lib
+fn get_local_ip_address() -> std::net::IpAddr {
+     match local_ip_address::local_ip() {
+            Ok(address) => {
+                address
+            }
+            Err(_) => {
+                // workaround for https://github.com/EstebanBorai/local-ip-address/issues/82
+                let network_interfaces = local_ip_address::list_afinet_netifas().unwrap();
+
+                for (name, ip) in network_interfaces.into_iter() {
+                    if ip.is_ipv4() && name.starts_with("en"){
+                        return ip;
+                    }
+                }
+                panic!("mac os workaround failed");
+            }
+     }
 }
