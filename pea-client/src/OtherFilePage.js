661cc5f9d1c6680fb23da135bdae8d2df7a8bd1f
Heshan Padmasiri <hpheshan@gmail.com>
2023-01-19 18:29:29 +0530

    Fix formatting

--- pea-client/src/OtherFilePage.js
+++ pea-client/src/OtherFilePage.js
@@ -2,32 +2,28 @@ import React from "react";
 import config from "./config.json";
 
 class OtherFilePage extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            files: props.files
-        }
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      files: props.files,
+    };
+  }
 
-    render() {
-        const { files } = this.state;
-        const file_tags = files.map((each) => {
-            return file_tag(each.name, each.id)
-        });
-        return (
-            <ul>
-                {file_tags}
-            </ul>
-        )
-    }
+  render() {
+    const { files } = this.state;
+    const file_tags = files.map((each) => {
+      return file_tag(each.name, each.id);
+    });
+    return <ul>{file_tags}</ul>;
+  }
 }
 
 function file_tag(filename, id) {
   const src = `${config.SERVER_URL}/content/${filename}`;
   return (
-      <li key={id}>
-        <a href={src}>{filename}</a>{" "}
-      </li>
+    <li key={id}>
+      <a href={src}>{filename}</a>{" "}
+    </li>
   );
 }
 export default OtherFilePage;
