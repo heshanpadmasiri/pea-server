91afd97e56c9d92aadbdd796e5b85be8acfa6c73
Heshan Padmasiri <hpheshan@gmail.com>
2023-01-22 20:37:56 +0530

    Fix static routing (#40)
    * Duplicate navbar
    
    * Fix routing

---
+++ pea-client/src/navbar.js
@@ -0,0 +1,29 @@
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import Container from "react-bootstrap/Container";
+import { LinkContainer } from "react-router-bootstrap";
+
+function navbar() {
+  return (
+    <Navbar bg="dark" variant="dark">
+      <Container>
+        <LinkContainer to="/">
+          <Navbar.Brand>PeaServer</Navbar.Brand>
+        </LinkContainer>
+        <Nav className="me-auto">
+          <LinkContainer to="/">
+            <Nav.Link>Home</Nav.Link>
+          </LinkContainer>
+          <LinkContainer to="/videos">
+            <Nav.Link>Videos</Nav.Link>
+          </LinkContainer>
+          <LinkContainer to="/other">
+            <Nav.Link>Other Files</Nav.Link>
+          </LinkContainer>
+        </Nav>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default navbar;
