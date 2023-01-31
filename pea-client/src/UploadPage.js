41429aad94631958efbdc87771ad8e8ddb90116f
Heshan Padmasiri <hpheshan@gmail.com>
2023-01-31 20:15:32 +0530

    Add progress bar

--- pea-client/src/UploadPage.js
+++ pea-client/src/UploadPage.js
@@ -9,20 +9,23 @@ import Container from "react-bootstrap/Container";
 
 function UploadPage() {
   const { register, handleSubmit } = useForm();
-  const [updateProgress, setUpdateProgress ] = useState(0);
+  const [uploadProgress, setUploadProgress ] = useState(0);
+  const [uploadActive, setUploadActive] = useState(false);
   const nav = navbar();
 
   const onSubmit = async (data) => {
+    setUploadActive(true);
     const formData = new FormData();
     formData.append("file", data.file[0]);
     const post_config = {
       onUploadProgress: progressEvent => {
           var percentCompleted = Math.round((progressEvent.loaded * 100) / progressEvent.total)
-          if (percentCompleted == 100) {
-            setUpdateProgress(0);
+          if (percentCompleted === 100) {
+            setUploadProgress(0);
+            setUploadActive(false);
           }
           else {
-            setUpdateProgress(percentCompleted);
+            setUploadProgress(percentCompleted);
           }
       },
       headers: {
@@ -32,19 +35,30 @@ function UploadPage() {
     axios
       .post(`${config.SERVER_URL}/file`, formData, post_config)
       .then((res) => console.log(res))
-      .catch((err) => console.error(err));
+      .catch((err) => {
+          console.error(err);
+          setUploadActive(false);
+      });
   };
 
   return (
     <Container className="p-3">
       {nav}
-      <p> Update progress {updateProgress}% </p>
+      {upload_progress(uploadProgress)}
       <form onSubmit={handleSubmit(onSubmit)}>
         <input type="file" {...register("file")} />
-        <input type="submit" />
+        <input disabled={uploadActive} type="submit" />
       </form>
     </Container>
   );
+}
+
+function upload_progress(progress) {
+    return (progress === 0) ? (<p>no active upload</p>) : 
+                             (<div> 
+                                <label for="upload">Upload progress {progress}%</label>
+                                <progress id="upload" value={progress} max="100"> {progress}% </progress>
+                              </div>)
 }
 
 export default UploadPage;
