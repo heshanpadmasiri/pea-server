19a17bd1a1da3028f0bc695a4ce4beb66bb1e639
Heshan Padmasiri <hpheshan@gmail.com>
2023-02-20 18:51:12 +0530

    Implement tag selection for images

--- pea-client/src/images.js
+++ pea-client/src/images.js
@@ -1,5 +1,8 @@
 import React from "react";
 import Container from "react-bootstrap/Container";
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Form from 'react-bootstrap/Form';
 import config from "./config.json";
 import navbar from "./navbar";
 import ImageGallery from "react-image-gallery";
@@ -8,15 +11,45 @@ class ImagePage extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      images: []
+      images: [],
+      tags: [],
+      selected_tags: [],
     };
   }
 
   componentDidMount() {
     this.get_images();
+    this.get_tags();
   }
 
+
   get_images() {
+    const { selected_tags } = this.state;
+    if (selected_tags.length === 0) {
+      this.get_images_without_tags();
+    } else {
+      this.get_images_with_tags(selected_tags);
+    }
+  }
+
+  get_images_with_tags(tags) {
+    const image_tys = ["jpg", "jpeg"];
+    Promise.all(image_tys.map(async (ty) => {
+      let res = await axios.post(`${config.SERVER_URL}/query`, {
+        data: {
+          ty,
+          tags
+        }
+      });
+      return res.data
+    })).then(image_array => {
+      this.setState({
+        images: image_array.flat()
+      })
+    })
+  }
+
+  get_images_without_tags() {
     const image_tys = ["jpg", "jpeg"];
     Promise.all(image_tys.map(async (ty) => {
       let res = await axios.get(`${config.SERVER_URL}/files/${ty}`);
@@ -28,17 +61,59 @@ class ImagePage extends React.Component {
     })
   }
 
+  get_tags() {
+    axios.get(`${config.SERVER_URL}/tags`).then((res) => {
+      this.setState({
+        tags: res.data
+      });
+    });
+  }
+
+  get_tag_switches() {
+    const { tags, selected_tags } = this.state;
+    return tags.map((tag, id) => {
+      return (
+        <Form.Check
+          type="switch"
+          id={id}
+          key={id}
+          label={tag}
+          checked={selected_tags.includes(tag)}
+          onChange={() => {
+            if (selected_tags.includes(tag)) {
+              const new_tags = selected_tags.filter((t) => t !== tag);
+              this.setState({
+                selected_tags: new_tags,
+              });
+            } else {
+              selected_tags.push(tag);
+              this.setState({
+                selected_tags,
+              });
+            }
+            this.get_images();
+          }}
+        />
+      );
+    });
+  }
+
   render() {
     const { images } = this.state;
-    console.log(images)
     const items = images.map((each) => {
       return { original: `${config.SERVER_URL}/content/${each.id}` };
     });
     const nav = navbar();
+    const tag_switches = this.get_tag_switches();
     return (
       <Container className="p-3">
         {nav}
-        <ImageGallery items={items} infinite={true} />
+        <Row>
+          <Col sm={2}>{tag_switches}</Col>
+          <Col sm={10}>
+            <ImageGallery items={items} infinite={true} />
+          </Col>
+        </Row>
       </Container>
     );
   }
