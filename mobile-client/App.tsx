15e30be6e5f58714c559aabcba3a3d5a349f56ff
Heshan Padmasiri <hpheshan@gmail.com>
2023-02-24 17:09:11 +0530

    Factor other files to separate component

--- mobile-client/App.tsx
+++ mobile-client/App.tsx
@@ -1,61 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
 import { ReactElement, useState } from 'react';
-import { StyleSheet, Text, View, FlatList } from 'react-native';
+import { StyleSheet, Text, View, FlatList, SafeAreaView, Platform } from 'react-native';
+import OtherFiles from './Components/OtherFiles';
 import { getFiles, Metadata } from './services';
 
 export default function App() {
-  const [isLoading, setIsLoading] = useState(true);
-  const [isError, setIsError] = useState(false);
-  const [files, setFiles] = useState<Metadata[]>([]);
-
-  const get_data = (setLoading: (value: boolean) => void,
-    setError: (value: boolean) => void, setFiles: (value: Metadata[]) => void) => {
-    getFiles().then((files) => {
-      setFiles(files);
-    }).catch((err) => {
-      console.error(err);
-      setError(true);
-    }).finally(() => {
-      setLoading(false);
-    })
-  }
-
-  get_data(setIsLoading, setIsError, setFiles);
-
-  if (isLoading) {
-    return (
-      <View style={styles.container}>
-        <Text>Loading...</Text>
-        <StatusBar style="auto" />
-      </View>
-    );
-  }
-  else if (isError) {
-    return (
-      <View style={styles.container}>
-        <Text>Error!</Text>
-        <StatusBar style="auto" />
-      </View>
-    )
-  }
-  else {
-    const data = files.map((file: Metadata) => {
-      return { key: file.name };
-    })
-    return (
-      <View style={styles.container}>
-        <FlatList data={data} renderItem={({ item }) => <Text>{item.key}</Text>} />
-        <StatusBar style="auto" />
-      </View>
-    )
-  }
+  return (
+    <View style={styles.container}>
+      <StatusBar />
+      <OtherFiles />
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flex: 1
   },
 });
---
+++ mobile-client/Components/OtherFiles.tsx
@@ -0,0 +1,59 @@
+import { useState } from "react";
+import { StyleSheet, Text, View, FlatList, SafeAreaView, Platform, StatusBar } from 'react-native';
+import { getFiles, Metadata } from "../services";
+
+export default function OtherFiles() {
+    const [isLoading, setIsLoading] = useState(true);
+    const [isError, setIsError] = useState(false);
+    const [files, setFiles] = useState<Metadata[]>([]);
+
+    const get_data = (setLoading: (value: boolean) => void,
+        setError: (value: boolean) => void, setFiles: (value: Metadata[]) => void) => {
+        getFiles().then((files) => {
+            setFiles(files);
+        }).catch((err) => {
+            console.error(err);
+            setError(true);
+        }).finally(() => {
+            setLoading(false);
+        })
+    }
+
+    get_data(setIsLoading, setIsError, setFiles);
+
+    if (isLoading) {
+        return (
+            <View style={styles.container}>
+                <Text>Loading...</Text>
+            </View>
+        );
+    }
+    else if (isError) {
+        return (
+            <View style={styles.container}>
+                <Text>Error!</Text>
+            </View>
+        )
+    }
+    else {
+        const data = files.map((file: Metadata) => {
+            return { key: file.name };
+        })
+        return (
+            <SafeAreaView style={styles.safeArea}>
+                <FlatList data={data} renderItem={({ item }) => <Text>{item.key}</Text>} />
+            </SafeAreaView>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1
+    },
+    safeArea: {
+        flex: 1,
+        paddingHorizontal: 10,
+        paddingTop: Platform.OS === "android" ? 25 : 0,
+    }
+});
