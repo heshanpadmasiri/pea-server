6156974cf787a5bd972c7d11bd5bc3a0ba65671e
Heshan Padmasiri <hpheshan@gmail.com>
2023-02-25 07:19:26 +0530

    Refactor the codebase to help UI creation (#75)
    * Factor other files to separate component
    
    * Factor state update out
    
    * Code formatting
    
    * Factor styles out

---
+++ mobile-client/Components/OtherFiles.tsx
@@ -0,0 +1,38 @@
+import { useState } from "react";
+import { StyleSheet, Text, View, FlatList, SafeAreaView, Platform } from 'react-native';
+import { getFiles, Metadata } from "../utils/services";
+import { get_file_data_and_update_state } from "../utils/states";
+import styles from "../utils/styles";
+
+export default function OtherFiles() {
+    const [isLoading, setIsLoading] = useState(true);
+    const [isError, setIsError] = useState(false);
+    const [files, setFiles] = useState<Metadata[]>([]);
+
+    get_file_data_and_update_state(getFiles, setFiles, setIsLoading, setIsError);
+
+    if (isLoading) {
+        return (
+            <View style={styles.container}>
+                <Text>Loading...</Text>
+            </View>
+        );
+    }
+    else if (isError) {
+        return (
+            <View style={styles.container}>
+                <Text>Error!</Text>
+            </View>
+        )
+    }
+    else {
+        const data = files.map((file: Metadata) => {
+            return { key: file.name };
+        })
+        return (
+            <SafeAreaView style={styles.safeArea}>
+                <FlatList data={data} renderItem={({ item }) => <Text>{item.key}</Text>} />
+            </SafeAreaView>
+        )
+    }
+}
