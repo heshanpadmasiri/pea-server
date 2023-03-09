17197712b789e2a50b4c46081d38cfd3fd530181
Heshan Padmasiri <hpheshan@gmail.com>
2023-03-09 18:52:37 +0530

    Improve video playback UX (#91)
    * Hide header
    
    * Make default video playback fullscreen

--- mobile-client/Components/VideoFiles/VideoFiles.tsx
+++ mobile-client/Components/VideoFiles/VideoFiles.tsx
@@ -19,7 +19,7 @@ export default function VideoFiles() {
     const Stack = createNativeStackNavigator<VideoRouteParamList>();
     return (
         <NavigationContainer independent={true}>
-            <Stack.Navigator>
+            <Stack.Navigator screenOptions={{headerShown: false}}>
                 <Stack.Screen name="Selector" component={VideoSelector} />
                 <Stack.Screen name="Player" component={VideoPlayer} />
             </Stack.Navigator>
@@ -56,12 +56,12 @@ type VideoPlayerProps = NativeStackScreenProps<VideoRouteParamList, 'Player'>;
 
 function VideoPlayer(props: VideoPlayerProps) {
     const uri = props.route.params.url;
-    const video = React.useRef(null);
+    const video = React.useRef<Video>(null);
+    video.current?.presentFullscreenPlayer();
     const setOrientation = () => {
-          ScreenOrientation.lockAsync(ScreenOrientation.OrientationLock.LANDSCAPE);
+        ScreenOrientation.lockAsync(ScreenOrientation.OrientationLock.LANDSCAPE);
     }
     return (
-        <View style={styles.container}>
             <Video
                 ref={video}
                 style={styles.video}
@@ -71,8 +71,8 @@ function VideoPlayer(props: VideoPlayerProps) {
                 useNativeControls
                 isLooping
                 onFullscreenUpdate={setOrientation}
+                onReadyForDisplay={() => {video.current?.presentFullscreenPlayer(); }}
             />
-        </View>
     );
 }
 
