644893f3046eb092a0fd46dfe54bac3bed4f1716
Heshan Padmasiri <hpheshan@gmail.com>
2023-03-24 16:41:48 +0530

    Change the fetch mock

--- client/tests/TagSelector.test.tsx
+++ client/tests/TagSelector.test.tsx
@@ -1,5 +1,5 @@
-import { fetch } from 'cross-fetch';
-const { Headers, Request, Response } = fetch;
+// import { fetch } from 'cross-fetch';
+// const { Headers, Request, Response } = fetch;
 
 import { rest } from 'msw';
 import { setupServer } from 'msw/node';
@@ -10,9 +10,9 @@ import { Provider } from 'react-redux';
 import TagSelector from '../Components/TagSelector';
 
 // No idea why I need these but some how even after giving a fetchFn I am getting errors without these
-global.Headers = Headers;
-global.Request = Request;
-global.Response = Response;
+// global.Headers = Headers;
+// global.Request = Request;
+// global.Response = Response;
 
 const BASE_URL = config.SERVER_URL;
 export const handlers = [
@@ -26,6 +26,7 @@ const server = setupServer(...handlers);
 beforeAll(() => server.listen());
 afterEach(() => server.resetHandlers());
 afterAll(() => server.close());
+const delay = (ms: number) => new Promise(res => setTimeout(res, ms));
 test('renders component', async () => {
     const store = setupStore();
     render(
@@ -33,7 +34,8 @@ test('renders component', async () => {
             <TagSelector />
         </Provider>
     );
-    const re = await screen.findByTestId('recieved-tag');
-    expect(re).toBeTruthy();
+    // This line is not working in github and due to some reason hanging in both local and github why?
+    await screen.findByTestId('recieved-tag');
+    // delay(1000); 
     expect(screen.toJSON()).toMatchSnapshot()
 });
